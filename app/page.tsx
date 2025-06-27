fce6e3f7c191b5d6bae6ab08542003b24266a05f
CarmenVega25 <vegacarmen8@gmail.com>
2025-06-27 10:09:24 -0700

    Use img instead of Image

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-white text-gray-800 font-sans">
       <header className="flex items-center px-6 py-4">
-        <Image
+        <img
           src="/App-Forge-Solutions/images/App-Forge-Solutions.png"
           alt="App Forge Solutions Logo"
           width={40}
